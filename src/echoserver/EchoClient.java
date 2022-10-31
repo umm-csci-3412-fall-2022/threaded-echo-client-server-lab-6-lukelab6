97f5003e054f565e2a62d6b39e1a6237b6cf62ed
burde041 <burde041@morris.umn.edu>
2022-10-30 22:29:28 -0500

    Implement EchoClient.java

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -8,16 +8,57 @@ import java.net.Socket;
 public class EchoClient {
 	public static final int PORT_NUMBER = 6013;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{
 		EchoClient client = new EchoClient();
 		client.start();
 	}
 
 	private void start() throws IOException {
+		try{
 		Socket socket = new Socket("localhost", PORT_NUMBER);
 		InputStream socketInputStream = socket.getInputStream();
 		OutputStream socketOutputStream = socket.getOutputStream();
+		
+		Thread firstThread = new Thread(EchoClient::readFromUser);
+		firstThread.start();
 
-		// Put your code here.
+		Thread secondThread = new Thread(EchoClient::readFromServer);
+		secondThread.start();
+
+		firstThread.join();
+		secondThread.join();
+
+		socket.close();
+		} catch (IOException ioe){
+			System.err.println("An error occurred:");
+			ioe.printStackTrace();
+		}
+	}
+
+	public static void readFromUser(){
+		int byteRead;
+		try{
+			while((byteRead = System.in.read()) != -1){
+				socketOut.write(byteRead);
+				socketOut.flush();
+			}
+			socket.shutdownOutput();
+		} catch(IOException ioe){
+			System.err.println("An error occurred:");
+			ioe.printStackTrace();
+		}
+	}
+
+	public static void readFromServer(){
+		int byteRead;
+		try{
+			while((byteRead = socketIn.read()) != -1){
+				System.out.write(byteRead);
+				System.out.flush();
+			}
+		} catch(IOException ioe){
+			System.err.println("An error occurred:");
+			ioe.printStackTrace();
+		}
 	}
 }
